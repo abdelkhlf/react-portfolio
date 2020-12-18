ed81009e8d7ce2480df8615d8dc73799f22e82b8
root <khaloufabdelrahmen@gmail.com>
2020-12-18 23:41:49 +0100

    ajout du build (projet compilé pret a etre deployé)

---
+++ build/precache-manifest.32fe9ddc7fd89c2a4ffe229d9f1283ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2d85be66ae1ca3c9c2212b17b64fff2",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "63d5154ab5e5b0966ef8",
+    "url": "/react-portfolio/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "f9090b7200918e2f0300",
+    "url": "/react-portfolio/static/js/2.9a12e492.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/react-portfolio/static/js/2.9a12e492.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63d5154ab5e5b0966ef8",
+    "url": "/react-portfolio/static/js/main.4accd972.chunk.js"
+  },
+  {
+    "revision": "ea608e5f497480426bb5",
+    "url": "/react-portfolio/static/js/runtime-main.7540febe.js"
+  }
+]);
